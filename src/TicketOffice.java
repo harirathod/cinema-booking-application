be9c11a6e4dbbde80afaeabe39f3dae1b77e62d7
harirathod <h4ri.rathod@gmail.com>
2022-12-24 17:10:14 +0000

    Add tests to JUnit

--- src/TicketOffice.java
+++ src/TicketOffice.java
@@ -19,15 +19,17 @@ public class TicketOffice
     }
 
     /**
-     * Add a screen to the cinema.
+     * Add a screen to the cinema. If the number of columns or row for the 
+     * screen is invalid (< 0), no screen will be added. If the screen already
+     * exists, another screen cannot be added.
      * @param id The id of the screen.
      * @param numberOfColumns The number of columns for the screen.
      * @param numberOfRows The number of rows for the screen.
-     * @return True if the screen was successfully added.
+     * @return True if the screen was successfully added, else false.
      */
     public boolean addScreen(int id, int numberOfColumns, int numberOfRows)
     {
-        if(numberOfColumns > 1 || numberOfRows < 0) {
+        if(numberOfColumns < 0 || numberOfRows < 0 || screens.get(id) != null) {
             return false;
         }
         screens.put(id, new Screen(id, numberOfColumns, numberOfRows));
@@ -35,13 +37,14 @@ public class TicketOffice
     }
     
     /**
-     * Add a screen to the cinema.
+     * Add a screen to the cinema. If the screen id has already been added,
+     * adding another screen with the same id will have no effect.
      * @param screen The screen to be added.
      * @return True if the screen was successfully added.
      */
     public boolean addScreen(Screen screen)
     {
-        if(screen == null) {
+        if(screen == null || screens.get(screen.getId()) != null) {
             return false;
         }
         screens.put(screen.getId(), screen);
@@ -59,7 +62,8 @@ public class TicketOffice
     }
     
     /**
-     * Show a new movie at a specific screen.
+     * Show a new movie at a specific screen. Does not add the movie if there
+     * is no screen as the provided id.
      * @param id The id of the screen we want to change the movie showing.
      * @param movieTitle The title of the movie we want the screen to show.
      * @param ticketCost The cost of a ticket in cents.
@@ -85,8 +89,9 @@ public class TicketOffice
     }
     
     /**
-     * Book a ticket for the provided movie.
-     * @param The title of the movie to book a ticket for.
+     * Book a random ticket for a movie.
+     * @param The movie to book a random ticket for.
+     * @return A ticket to the movie, chosen at random.
      */
     public Ticket bookRandomTicket(String movieTitle)
     {
@@ -94,6 +99,21 @@ public class TicketOffice
                     .map(id -> screens.get(id))
                     .filter(screen -> movieTitle.equals(screen.getMovieTitle()))
                     .map(screen -> screen.getRandomTicket())
+                    .findFirst()
+                    .orElse(null);
+    }
+    
+    /**
+     * Book a ticket for a movie.
+     * @param The movie to book a ticket for.
+     * @return A ticket to the movie, chosen at random.
+     */
+    public Ticket bookTicket(String movieTitle, int seatNumber, int rowNumber)
+    {
+        return screens.keySet().stream()
+                    .map(id -> screens.get(id))
+                    .filter(screen -> movieTitle.equals(screen.getMovieTitle()))
+                    .map(screen -> screen.getTicket(seatNumber, rowNumber))
                     .findFirst()
                     .orElse(null);
     }
