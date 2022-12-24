c3cbe11823e185cb07625aef831e9ae517a6e719
harirathod <h4ri.rathod@gmail.com>
2022-12-24 15:26:34 +0000

    Rename directory

---
+++ src/Screen.java
@@ -0,0 +1,184 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.time.LocalDateTime;
+
+/**
+ * Class Screen represents a screen in a multiplex cinema.
+ *
+ * @author Hari Rathod k22002783
+ * @version 2022.12.08
+ */
+public class Screen
+{
+    private String movieTitle;
+    private int ticketCost;
+    private int availableSeats;
+    private Seat[][] seats;
+    private int id;
+
+    /**
+     * Constructor for objects of class Screen. Each row has a fixed length of
+     * 20 seats.
+     * If the number of seats for a screen is 5, then the screen will contain
+     * 1 row, with 20 seats. If the number of seats provided is 21, the screen
+     * will contain 2 rows, each with 20 seats (40 seats total).
+     * @param numberOfSeats The minimum number of seats the screen should have.
+     */
+    public Screen(int id, int numberOfColumns, int numberOfRows)
+    {
+        this.id = id;
+        if(numberOfColumns < 1 || numberOfRows < 1) {
+            this.seats = new Seat[0][0];
+            return;
+        }
+        seats = new Seat[numberOfColumns][numberOfRows];
+        for(int i = 0; i < seats.length; i++) {
+            for(int  j = 0; j < seats[i].length; j++) {
+                seats[i][j] = new Seat();
+            }
+        }
+    
+        emptyScreen();
+    }
+    
+    /**
+     * Empty the screen - make every seat available.
+     */
+    public void emptyScreen()
+    {
+        for(int i = 0; i < seats.length; i++) {
+            for(int  j = 0; j < seats[i].length; j++) {
+                seats[i][j].setAvailable();
+            }
+        }
+        availableSeats = getNumberOfSeats();
+    }
+    
+    /**
+     * Get number of available seats.
+     */
+    public int getNumberOfAvailableSeats()
+    {
+        return availableSeats;
+    }
+    
+    /**
+     * Get the total number of seats.
+     * @return The total number of seats.
+     */
+    public int getNumberOfSeats()
+    {
+        return seats.length * seats[0].length;
+    }
+    
+    /**
+     * Take the seat's seat and row number, and mark that seat as booked.
+     * Return false if the seat was already booked. Return true if the seat
+     * was not already booked, to show the booking has been confirmed.
+     * @return True if the booking was successful, false if it was not (if
+     * the seat was already booked).
+     */
+    public boolean book(int seatNumber, int rowNumber)
+    {
+        // entering a value of 1 should access element 0 of the array
+        seatNumber = seatNumber - 1;
+        rowNumber = rowNumber - 1; 
+        
+        if(seatNumber < 0 || seatNumber > seats.length - 1 
+        || rowNumber < 0 || rowNumber > seats[0].length - 1) {
+            return false;
+        }
+        if(!seats[seatNumber][rowNumber].isAvailable()) {
+            return false;
+        }
+        seats[seatNumber][rowNumber].setUnavailable();
+        availableSeats--;
+        return true;
+    }
+    
+    /**
+     * Change the screen to show a new movie, with a new ticket cost.
+     * @param newMovieTitle The new title of the movie.
+     * @param ticketCost The cost of a ticket (in cents) to see this movie.
+     */
+    public void addNewMovie(String newMovieTitle, int ticketCost)
+    {
+        movieTitle = newMovieTitle;
+        this.ticketCost = ticketCost;
+        emptyScreen();
+    }
+    
+    /**
+     * Get the details of the screen, including id, current movie, and ticket
+     * cost.
+     * @return The details of the screen.
+     */
+    public String getDetails()
+    {
+        String details = "";
+        details += "ID: " + id + "\n";
+        details += "Current Movie: " + movieTitle + "\n";
+        details += "Ticket Cost: " + ticketCost + "\n";
+        return details;
+    }
+    
+    /**
+     * Get a random ticket.
+     * @return A ticket object if there is at least one available seat, 
+     * otherwise return null.
+     */
+    public Ticket getRandomTicket()
+    {
+        if(seats == null) {
+            return null;
+        }
+        int randomRowNumber = 0;
+        int randomSeatNumber = 0;
+        
+        do {
+            randomRowNumber = new Random().nextInt(seats[0].length);
+            randomSeatNumber = new Random().nextInt(seats.length);
+        } while(!seats[randomSeatNumber][randomRowNumber].isAvailable());
+        
+        book(randomSeatNumber + 1, randomRowNumber + 1);
+    
+        return new Ticket(id, movieTitle, randomSeatNumber + 1, randomRowNumber + 1,
+        ticketCost, LocalDateTime.now());
+    }
+    
+    /*
+     * Get a best ticket (a ticket closest to the centre of the room).
+     * @return A ticket closest to the centre of the room.
+     */
+    /*public Ticket getBestTicket()
+    {
+        
+    }*/
+    
+    /**
+     * Get the title of the movie showing in a screen.
+     * @return The title of the movie being shown.
+     */
+    public String getMovieTitle()
+    {
+        return movieTitle;
+    }
+    
+    /**
+     * Get the id of the screen.
+     * @return The id of the screen.
+     */
+    public int getId()
+    {
+        return id;
+    }
+    
+    /**
+     * Get the cost (in cents) of a ticket for the movie.
+     * @return The cost of a ticket to see the movie.
+     */
+    public int getTicketCost()
+    {
+        return ticketCost;
+    }
+}
---
+++ src/ScreenTest.java
@@ -0,0 +1,123 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class ScreenTest.
+ *
+ * @author  Hari Rathod k22002783
+ * @version 2022.12.08
+ */
+public class ScreenTest
+{
+    Screen screen1 = new Screen(0, 0, 0);
+    Screen screen2 = new Screen(-1, 0, 0);
+    Screen screen3 = new Screen(1, 20, 1);
+    Screen screen4 = new Screen(2, 30, 2);
+    
+    /**
+     * Default constructor for test class ScreenTest
+     */
+    public ScreenTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    
+    /**
+     * Test if the book method works correctly.
+     */
+    @Test
+    public void testBook()
+    {
+        assertEquals(false, screen1.book(0, 0));
+        assertEquals(true, screen3.book(15,1));
+        assertEquals(true, screen4.book(23,2));
+    }
+
+    /**
+     * Test 'get number of seats'.
+     */
+    @Test
+    public void testGetNumberOfSeats()
+    {
+        assertEquals(0, screen1.getNumberOfAvailableSeats());
+        assertEquals(0, screen2.getNumberOfAvailableSeats());
+        assertEquals(20, screen3.getNumberOfAvailableSeats());
+        assertEquals(60, screen4.getNumberOfAvailableSeats());
+    }
+    
+    /**
+     * Test 'get id'.
+     */
+    @Test
+    public void testGetId()
+    {
+        assertEquals(0, screen1.getId());
+        assertEquals(-1, screen2.getId());
+    }
+    
+    /**
+     * Test if the available seats are calculated correctly.
+     */
+    @Test
+    public void testGetNumberOfAvailableSeats()
+    {
+        screen4.book(19, 2);
+        assertEquals(59, screen4.getNumberOfAvailableSeats());
+        
+        screen3.book(-1, 3);
+        assertEquals(20, screen3.getNumberOfAvailableSeats());
+    }
+    
+    /**
+     * Test if a new movie title is added correctly.
+     */
+    @Test
+    public void testAddNewMovie()
+    {
+        screen4.addNewMovie("Shazam 3", 900);
+        assertEquals("Shazam 3", screen4.getMovieTitle());
+        assertEquals(900, screen4.getTicketCost());
+        
+        screen3.addNewMovie("Black Panther: Return of the Jedi", 1300);
+        assertEquals("Black Panther: Return of the Jedi", screen3.getMovieTitle());
+        assertEquals(1300, screen3.getTicketCost());
+    }
+    
+    /**
+     * Test if the empty screen method works correctly (should free up all seats).
+     */
+    @Test
+    public void testEmptyScreen()
+    {
+        screen4.book(1,1);
+        screen4.book(6,2);
+        screen4.book(13,1);
+        assertEquals(60, screen4.getNumberOfSeats());
+        assertEquals(57, screen4.getNumberOfAvailableSeats());
+        screen4.emptyScreen();
+        assertEquals(60, screen4.getNumberOfAvailableSeats());
+    }
+}
+
+
