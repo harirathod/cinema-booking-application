07c9f7a36ade08466f8c195713c4a26f6c24b912
harirathod <h4ri.rathod@gmail.com>
2023-01-31 22:16:55 +0000

    Update Parser to recognise two parts of an input string: a command word, and a subject of a command

--- src/Parser.java
+++ src/Parser.java
@@ -4,6 +4,8 @@
  * @version 2023.01.31
  */
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Parser {
@@ -13,7 +15,7 @@ public class Parser {
 
     public static void main(String[] args) {
         Parser parser = new Parser();
-        System.out.println(parser.readInput());
+        parser.readInput();
     }
 
     public Parser() {
@@ -26,18 +28,24 @@ public class Parser {
      */
     public Command readInput()
     {
+        String commandWord = null;
+        String secondWord = null;
+
         System.out.print("> ");
-        String input = scanner.nextLine();      // process the next line
-        input = input.toLowerCase();
-        Scanner readInput = new Scanner(input);
-        String commandWord = null;              // first word defines the command
-        String secondWord = null;               // second word defines the object / subject of the command
-        if(readInput.hasNext()) {
-            commandWord = readInput.next();
-            if(readInput.hasNext()) {
-                secondWord = readInput.next();
+        String inputLine = scanner.nextLine().toLowerCase();      // process the next line
+
+        // split the input string into 2 parts: the command, and the subject of the command
+        String[] inputWords = inputLine.trim().split(" ", 2);
+        for(int i = 0; i < inputWords.length; i++) {
+            inputWords[i] = inputWords[i].trim();
+        }
+        if(inputWords.length > 0) {
+            commandWord = inputWords[0];        // first word defines the command
+            if(inputWords.length > 1) {
+                secondWord = inputWords[1];     // second word defines the object / subject of the command
             }
         }
+
         return new Command(CommandWordConverter.toCommandWord(commandWord), secondWord);
     }
 }
