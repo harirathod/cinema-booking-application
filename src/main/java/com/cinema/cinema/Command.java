c100820faa8b86aac486d2e7ded916fd599669d9
harirathod <h4ri.rathod@gmail.com>
2023-03-29 21:59:30 +0100

    Review Command class

--- src/main/java/com/cinema/cinema/Command.java
+++ src/main/java/com/cinema/cinema/Command.java
@@ -1,15 +1,20 @@
 package com.cinema.cinema;
 /**
- * This class defines the format for a complete command, consisting of a command word and a second word. The command word
- * defines the action to be done, e.g., 'BOOK', and the second word defines the object upon which to do the action, e.g.,
- * 'movie'. This gives a complete command as 'BOOK movie'.
+ * This class defines the format for a Command, consisting of a command (first) word and a second word.
+ * The command (first) word defines the action to be done, e.g., 'BOOK', and the second word defines the object upon which to do the action, e.g.,
+ * 'movie'. This gives a Command as 'BOOK movie'.
  * @author Hari Rathod
- * @version 2023.01.31
+ * @version 2023.03.29
  */
 public class Command {
     private CommandWord commandWord;
     private String secondWord;
 
+    /**
+     * Initialise fields
+     * @param commandWord The first word of the Command. The 'action'.
+     * @param secondWord The second word of the Command. The 'subject'.
+     */
     public Command(CommandWord commandWord, String secondWord)
     {
         this.commandWord = commandWord;
@@ -17,8 +22,8 @@ public class Command {
     }
 
     /**
-     * Get the command word (the first word) associated with a complete command.
-     * @return The command word (the first word).
+     * Get the command word.
+     * @return The command word.
      */
     public CommandWord getCommandWord()
     {
@@ -26,8 +31,8 @@ public class Command {
     }
 
     /**
-     * Get the second word (the subject of the command) associated with a complete command.
-     * @return The second word (the subject of the command).
+     * Get the second word.
+     * @return The second word.
      */
     public String getSecondWord()
     {
@@ -36,7 +41,7 @@ public class Command {
 
     /**
      * Check if the command has a second word.
-     * @return True if the command has a second word, false if it doesn't have a second word or if the second word is null.
+     * @return True if the command has a second word. False if it doesn't have a second word or if the second word is null.
      */
     public boolean hasSecondWord()
     {
