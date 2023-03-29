4864b9f02c8be89170ff1437e29801fbc4f06319
harirathod <h4ri.rathod@gmail.com>
2023-03-29 21:55:40 +0100

    Refactor Parser to make it more general and less coupled to Command

--- src/main/java/com/cinema/cinema/Parser.java
+++ src/main/java/com/cinema/cinema/Parser.java
@@ -19,29 +19,13 @@ public class Parser {
     }
 
     /**
-     * Read the next line of input and convert the String input to a complete main.cinema.Command.
-     * @return The next line, in the format of a complete main.cinema.Command.
+     * Read the next line of input and return it.
+     * @return The next line of input.
      */
-    public Command readInput()
+    public String readInput()
     {
-        String commandWord = null;               // first word defines the command
-        String secondWord = null;               // second word defines the object (or subject) of the command
-
-        System.out.print("> ");
-        String inputLine = scanner.nextLine().toLowerCase();      // process the next line
-
-        // split the input string into 2 parts: the command, and the subject of the command
-        String[] inputWords = inputLine.trim().split(" ", 2);
-        for(int i = 0; i < inputWords.length; i++) {
-            inputWords[i] = inputWords[i].trim();
-        }
-        if(inputWords.length > 0) {             // prevent IndexOutOfBoundsException, by checking how many words have been provided
-            commandWord = inputWords[0];
-            if (inputWords.length > 1) {
-                secondWord = inputWords[1];
-            }
-        }
-        return new Command(CommandWordConverter.toCommandWord(commandWord), secondWord);
+        System.out.println("> ");
+        return scanner.nextLine();
     }
 
     /**
