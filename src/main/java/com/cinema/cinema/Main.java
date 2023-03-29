68525225078bec2f6fc3849ad675e6307aa767f1
harirathod <h4ri.rathod@gmail.com>
2023-03-29 21:55:06 +0100

    Remove spacing

--- src/main/java/com/cinema/cinema/Main.java
+++ src/main/java/com/cinema/cinema/Main.java
@@ -17,6 +17,7 @@ public class Main {
         // what should be here is a 'Controller' that starts the parser.
         CustomerBooking booking = new CustomerBooking();
         booking.start();
-
     }
+
+
 }
