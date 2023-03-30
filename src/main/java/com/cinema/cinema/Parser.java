1026c47811047ceac580c414860baf614c662be8
harirathod <h4ri.rathod@gmail.com>
2023-03-30 10:55:40 +0100

    Review Parser class

--- src/main/java/com/cinema/cinema/Parser.java
+++ src/main/java/com/cinema/cinema/Parser.java
@@ -1,16 +1,18 @@
-package com.cinema.cinema; /**
- * This class processes individual lines of user input. It converts the input from a String into a main.cinema.Command, and returns it.
+package com.cinema.cinema;
+
+/**
+ * This class processes individual lines of user input.
  * @author Hari Rathod
- * @version 2023.02.05
+ * @version 2023.03.30
  */
 
 import java.util.Scanner;
 
 public class Parser {
-    private Scanner scanner;                // scanner to read input
+    private Scanner scanner;
 
     /**
-     * Constructor to initialise the fields.
+     * Initialise fields.
      */
     public Parser() {
         scanner = new Scanner(System.in);
@@ -18,7 +20,7 @@ public class Parser {
 
     /**
      * Read the next line of input and return it.
-     * @return The next line of input.
+     * @return The next line of input, as a String.
      */
     public String readInput()
     {
@@ -33,14 +35,16 @@ public class Parser {
     public String getAllCommands()
     {
         String formattedString = "Commands available:";
+
         for(CommandWord command : CommandWord.values()) {
-            if(command.toString() != null) {
-                formattedString += "\n" + command.toString();
-                if(command.getPlaceholder() != null) {
+            if(command.getCommandString() != null) {
+                formattedString += "\n" + command.getCommandString();
+                if(command.hasPlaceholder()) {
                     formattedString += " <" + command.getPlaceholder() + ">";
                 }
             }
         }
+
         return formattedString;
     }
 }
