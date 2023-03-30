f5bc3fdd9b126d757a72975ddea3d87d8285830b
harirathod <h4ri.rathod@gmail.com>
2023-03-30 10:42:47 +0100

    Review CommandWord class

--- src/main/java/com/cinema/cinema/CommandWord.java
+++ src/main/java/com/cinema/cinema/CommandWord.java
@@ -3,7 +3,7 @@ package com.cinema.cinema;
 /**
  * This enumeration class defines the command words available.
  * @author Hari Rathod
- * @version 2023.01.31
+ * @version 2023.03.30
  */
 
 public enum CommandWord {
@@ -12,6 +12,12 @@ public enum CommandWord {
 
     private String commandString;
     private String placeholder;
+
+    /**
+     * Initialise fields.
+     * @param commandString The main part of the CommandWord.
+     * @param placeholder The placeholder.
+     */
     CommandWord(String commandString, String placeholder)
     {
 
@@ -23,18 +29,27 @@ public enum CommandWord {
      * Get the command string associated with a command word.
      * @return The command string associated with a command word.
      */
-    public String toString()
+    public String getCommandString()
     {
         return commandString;
     }
 
     /**
-     * Get the placeholder associated with a command word.
+     * Get the placeholder associated with a command word. Returns null
      * @return The placeholder associated with a command word.
      */
     public String getPlaceholder()
     {
         return placeholder;
+    }
+
+    /**
+     * Check whether a CommandWord has an associated placeholder or not.
+     * @return True if it has a placeholder, false otherwise.
+     */
+    public boolean hasPlaceholder()
+    {
+        return placeholder != null;
     }
 
 }
