77148476792049bc291cf40293df1b2750ade0db
harirathod <h4ri.rathod@gmail.com>
2023-04-05 14:26:46 +0100

    Add another Screen

--- src/main/java/com/cinema/cinema/SetUpDatabase.java
+++ src/main/java/com/cinema/cinema/SetUpDatabase.java
@@ -14,11 +14,13 @@ public class SetUpDatabase {
      */
     public static void main(String[] args) {
         try {
-            DatabaseManipulator databaseManipulator = new DatabaseManipulator();
+            ScreenDataManipulator screenDataManipulator = new ScreenDataManipulator();
             Screen screen = new Screen(1, 10, 12);
+            Screen screen2 = new Screen(2, 20, 32);
             screen.addNewMovie("Batman", 1300);
-            databaseManipulator.addScreen(screen);
-            databaseManipulator.addScreen(new Screen(2, 20, 32));
+            screen2.addNewMovie("Shazam | Revenge of the Sock", 2100);
+            screenDataManipulator.addScreen(screen);
+            screenDataManipulator.addScreen(screen2);
         }
         catch (IOException | ClassNotFoundException e) {
             System.out.println("Error loading database.");
