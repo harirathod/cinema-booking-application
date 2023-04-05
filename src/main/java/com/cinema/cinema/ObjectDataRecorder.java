c6bc316644ac8e9eaa2a5462b16eaa8e658e68b9
harirathod <h4ri.rathod@gmail.com>
2023-04-05 14:09:52 +0100

    Change documentation and add method for getting FILENAME

--- src/main/java/com/cinema/cinema/ObjectDataRecorder.java
+++ src/main/java/com/cinema/cinema/ObjectDataRecorder.java
@@ -7,59 +7,60 @@ import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.time.LocalDateTime;
 import java.util.*;
 
- /**
+/**
  * This class writes serializable objects to a file. Multiple objects can be written to the file.
  * This means that all objects are stored in List,
  * and this List is then written to the file.
  * This allows objects to be stored persistently.
+ *
  * @author hari_rathod
  * @version 2023.04.03
  */
-public class ObjectRecorder<E> {
-    private final String FILENAME;
-    public final Class<E> type;
+public class ObjectDataRecorder<E> {
+    private final Filename FILENAME;
+    private final Class<E> type;
 
     /**
-     * Constructor for ObjectRecorder.
+     * Constructor for ObjectDataRecorder.
+     *
      * @param filename The name of the file to write objects to.
      */
-    public ObjectRecorder(String filename, Class<E> type) {
+    public ObjectDataRecorder(Filename filename, Class<E> type) {
         this.FILENAME = filename;
         this.type = type;
     }
 
     /**
      * Write (append) an object to the file.
+     *
      * @param object The object to be written.
-     * @throws IOException If there was an error reading from or writing to the file.
+     * @throws IOException            If there was an error reading from or writing to the file.
      * @throws ClassNotFoundException If the file written to already has objects, of a different type,
-     * to the object provided here.
+     *                                to the object provided here.
      */
-    public void writeToFile(E object) throws IOException, ClassNotFoundException
-    {
+    public void writeToFile(E object) throws IOException, ClassNotFoundException {
         // Get the existing list of objects in the file and add this object to the list.
         List<E> objects = readListOfObjectsFromFile();
         objects.add(object);
 
         // Write the list of objects back to the file.
         try (ObjectOutputStream outputStream = new ObjectOutputStream(
-                Files.newOutputStream(Path.of(FILENAME)))) {
+                Files.newOutputStream(Path.of(FILENAME.toString())))) {
             outputStream.writeObject(objects);
         }
     }
 
     /**
      * Get the list of objects currently stored in the file.
+     *
      * @return The list of objects currently being stored in the file.
-     * @throws IOException If there was an error reading from the file.
-     * @throws ClassNotFoundException If the objects read were not of the same type as this ObjectRecorder's generic.
+     * @throws IOException            If there was an error reading from the file.
+     * @throws ClassNotFoundException If the objects read were not of the same type as this ObjectDataRecorder's generic.
      */
-    private List<E> readListOfObjectsFromFile() throws IOException, ClassNotFoundException
-    {
-        Path path = Path.of(FILENAME);
+    public List<E> readListOfObjectsFromFile() throws IOException, ClassNotFoundException {
+        Path path = Path.of(FILENAME.toString());
         try (ObjectInputStream inputStream = new ObjectInputStream(
                 Files.newInputStream(path))) {
             try {
@@ -76,9 +77,10 @@ public class ObjectRecorder<E> {
     }
 
     /**
-     * Validates that the objects in the list read are of the same type as this ObjectRecorder's type.
+     * Validates that the objects in the list read are of the same type as this ObjectDataRecorder's type.
+     *
      * @param objects The objects to be validated.
-     * @throws ClassNotFoundException If the objects in the file are not of the same type as the ObjectRecorder's type.
+     * @throws ClassNotFoundException If the objects in the file are not of the same type as the ObjectDataRecorder's type.
      */
     private void validateTypeOfList(List<?> objects) throws ClassNotFoundException {
         for (Object object : objects) {
@@ -91,11 +93,11 @@ public class ObjectRecorder<E> {
 
     /**
      * Creates a new file if it doesn't exist, otherwise, this method clears the existing file.
+     *
      * @throws IOException If there was an error handling the file.
      */
-    private void resetFile() throws IOException
-    {
-        Path path = Path.of(FILENAME);
+    public void resetFile() throws IOException {
+        Path path = Path.of(FILENAME.toString());
         if (Files.exists(path)) {
             try (ObjectOutputStream outputStream = new ObjectOutputStream(
                     Files.newOutputStream(path, StandardOpenOption.TRUNCATE_EXISTING))) {
@@ -104,5 +106,14 @@ public class ObjectRecorder<E> {
         } else {
             Files.createFile(path);
         }
+    }
+
+    /**
+     * Get the filename that this class is writing to.
+     *
+     * @return The filename being written to.
+     */
+    public Filename getFILENAME() {
+        return FILENAME;
     }
 }
