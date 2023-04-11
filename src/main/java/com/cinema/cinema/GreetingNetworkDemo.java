941f9dbca73325cd5fbd7020c8b1f3a53b1513dd
harirathod <h4ri.rathod@gmail.com>
2023-04-11 19:04:52 +0100

    Demo for networking

--- src/main/java/com/cinema/cinema/GreetingNetworkDemo.java
+++ src/main/java/com/cinema/cinema/GreetingNetworkDemo.java
@@ -3,8 +3,10 @@ package com.cinema.cinema;
 public class GreetingNetworkDemo {
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         GreetingServer greetingServer = new GreetingServer();
 
         GreetingClient.start();
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
