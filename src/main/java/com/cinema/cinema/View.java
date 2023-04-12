52e04e0d632dee601629131b4c40fb0f5509a6d9
harirathod <h4ri.rathod@gmail.com>
2023-04-12 14:20:03 +0100

    Move getSeparator method and add documentation

--- src/main/java/com/cinema/cinema/View.java
+++ src/main/java/com/cinema/cinema/View.java
@@ -1,7 +1,21 @@
 package com.cinema.cinema;
 
+/**
+ * A view interface for the cinema booking application.
+ * @author hari_rathod
+ * @version 2023.04.12
+ */
 public interface View {
     void displayWithFormatting(String message);
     void displayError(String message);
     void display(String message);
+
+    /**
+     * Return a line of dashes, that can be used to separate sections of text. Use to increase readability.
+     * @return A line of dashes, to separate sections of text.
+     */
+    default String getSeparator()
+    {
+        return "    ---------------------------------    ";
+    }
 }
