2977cbff060357d869f76767d6d5fb890c86853b
harirathod <h4ri.rathod@gmail.com>
2023-04-14 23:08:52 +0100

    Add documentation and add start(), getBlockingQueue(), various display methods, and setWaitingForInput()

--- src/main/java/com/cinema/cinema/View.java
+++ src/main/java/com/cinema/cinema/View.java
@@ -1,14 +1,53 @@
 package com.cinema.cinema;
 
+import java.io.InputStream;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * A view interface for the cinema booking application.
  * @author hari_rathod
  * @version 2023.04.12
  */
 public interface View {
-    void displayWithFormatting(String message);
+
+    /**
+     * Starts the view.
+     */
+    void start();
+
+    /**
+     * Get the BlockingQueue in use by this View (UI).
+     * This blocking queue is necessary to correctly pass user input between the View and the
+     * 'controller' (MVC) class.
+     * @return The BlockingQueue that will be used by the 'controller' (MVC) class to take user input.
+     */
+    BlockingQueue<String> getBlockingQueue();
+
+    /**
+     * Display some formatted text to the user.
+     * @param text The text to be displayed.
+     */
+    void displayWithFormatting(String text);
+
+    /**
+     * Display an error message to the user.
+     * @param message The error message to be displayed.
+     */
     void displayError(String message);
-    void display(String message);
+
+    /**
+     * Display an error message to the user, with both a title and content / body.
+     * @param title The title of the error message.
+     * @param message The content of the error message to be displayed.
+     */
+    void displayError(String title, String message);
+
+    /**
+     * Display some text to the user.
+     * @param text The text to be displayed.
+     */
+    void display(String text);
 
     /**
      * Return a line of dashes, that can be used to separate sections of text. Use to increase readability.
@@ -18,4 +57,9 @@ public interface View {
     {
         return "    ---------------------------------    ";
     }
+
+    /**
+     * Set the View as ready to receive input.
+     */
+    void setWaitingForInput();
 }
