57e84a243dca35487121939621747a511a13752d
harirathod <h4ri.rathod@gmail.com>
2023-04-14 23:06:40 +0100

    Make minor adjustments to Strings used

--- src/main/java/com/cinema/cinema/SetUpDatabase.java
+++ src/main/java/com/cinema/cinema/SetUpDatabase.java
@@ -18,12 +18,12 @@ public class SetUpDatabase {
             Screen screen = new Screen(1, 10, 12);
             Screen screen2 = new Screen(2, 20, 32);
             screen.addNewMovie("Batman", 1300);
-            screen2.addNewMovie("Shazam | Revenge of the Sock", 2100);
+            screen2.addNewMovie("Shazam", 2100);
             screenDataManipulator.addScreen(screen);
             screenDataManipulator.addScreen(screen2);
         }
         catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error loading database.");
+            System.out.println("Error loading database. " + e.getMessage());
         }
 
     }
