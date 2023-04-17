6e8a45d43d5ae35e75573b64fb67461cb839d096
harirathod <h4ri.rathod@gmail.com>
2023-04-17 12:39:46 +0100

    Use CountDownLatch to get instance of GuiView after Application.launch()
    As Application.launch() does not return a reference to the instance launch, this CountDownLatch helps us to apply the Singleton pattern, and allows us to perform operations such as view.display() without returning NullPointerException.

--- src/main/java/com/cinema/cinema/GuiView.java
+++ src/main/java/com/cinema/cinema/GuiView.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -17,44 +18,56 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class GuiView extends Application implements View {
 
+    // A reference to the current GuiView object. Allows the GuiView object to be accessed, even after being launched.
+    private static GuiView thisGuiView;
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
+
     // To display messages.
     private Label output;
 
     // To receive user input.
     private TextField input;
     private Button submitButton;
-    private BlockingQueue<String> blockingQueue;
+    //private BlockingQueue<String> blockingQueue;
 
     /**
-     * Sets up the view.
+     * Constructor.
+     */
+    public GuiView() {
+        thisGuiView = this;
+        countDownLatch.countDown();
+    }
+
+    /**
+     * Launches this JavaFX class.
      */
     @Override
-    public void start()
-    {
+    public void start() {
         launch("");
     }
 
     /**
      * Get the input from the user.
+     *
      * @return The user input.
      */
-    public String getInput()
-    {
-        return null; //TODO
+    public String getInput() {
+        return "testing";
     }
 
     /**
      * The method that starts the GUI application.
+     *
      * @param primaryStage the primary stage for this application, onto which
-     * the application scene can be set.
-     * Applications may create other stages, if needed, but they will not be
-     * primary stages.
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
      * @throws Exception If there was an error in starting the application.
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        blockingQueue = new LinkedBlockingQueue<>();
+        //blockingQueue = new LinkedBlockingQueue<>();
 
         output = new Label();
 
@@ -63,9 +76,10 @@ public class GuiView extends Application implements View {
         submitButton = new Button("Enter text");
         submitButton.setOnAction(e -> {
             String text = input.getText();
+            input.clear();
             display(text);
-            blockingQueue.offer(text);
-            submitButton.setDisable(true);
+            //blockingQueue.offer(text);
+            //submitButton.setDisable(true);
             //if(!blockingQueue.offer(text)) displayError("There was an error reading your input"
             //        , "Apologies, we could not process your user input at this time.");
         });
@@ -89,11 +103,11 @@ public class GuiView extends Application implements View {
 
     /**
      * Display some text to the user, by appending this text to the end of the 'output label'.
+     *
      * @param text The text to be displayed.
      */
     @Override
-    public void displayWithFormatting(String text)
-    {
+    public void displayWithFormatting(String text) {
         display(getSeparator());
         display(text);
         display(getSeparator());
@@ -101,11 +115,11 @@ public class GuiView extends Application implements View {
 
     /**
      * Display an error dialog to the user, with no header text but only content text.
+     *
      * @param message The error message to be displayed.
      */
     @Override
-    public void displayError(String message)
-    {
+    public void displayError(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setContentText(message);
         alert.showAndWait();
@@ -113,12 +127,12 @@ public class GuiView extends Application implements View {
 
     /**
      * Display an error dialog to the user, with both header and content text.
-     * @param title The title of the error message.
+     *
+     * @param title   The title of the error message.
      * @param message The content of the error message.
      */
     @Override
-    public void displayError(String title, String message)
-    {
+    public void displayError(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setHeaderText(title);
         alert.setContentText(message);
@@ -127,11 +141,26 @@ public class GuiView extends Application implements View {
 
     /**
      * Display some text to the user, by appending this text to the end of the 'output label'.
+     *
      * @param text The text to be displayed.
      */
     @Override
-    public void display(String text)
-    {
+    public void display(String text) {
         output.setText(output.getText() + "\n" + text);
     }
+
+    /**
+     * Get the instance of the GuiView currently running.
+     * @return The instance of the GuiView class that is running.
+     */
+    public static GuiView getInstance()
+    {
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return thisGuiView;
+    }
 }
+
