793d710e293f647f2d13e70db8fcded41e29f2cd
harirathod <h4ri.rathod@gmail.com>
2023-04-17 14:23:50 +0100

    Add functionality that when ENTER key is clicked, it submits the text

--- src/main/java/com/cinema/cinema/GuiView.java
+++ src/main/java/com/cinema/cinema/GuiView.java
@@ -5,7 +5,12 @@ import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.util.concurrent.BlockingQueue;
@@ -22,6 +27,7 @@ public class GuiView extends Application implements View {
     // A reference to the current GuiView object. Allows the GuiView object to be accessed, even after being launched.
     private static GuiView thisGuiView;
     private static CountDownLatch countDownLatch = new CountDownLatch(1);
+    private static int count = 10;
 
     // To display messages.
     private Label output;
@@ -29,7 +35,7 @@ public class GuiView extends Application implements View {
     // To receive user input.
     private TextField input;
     private Button submitButton;
-    //private BlockingQueue<String> blockingQueue;
+    private BlockingQueue<String> blockingQueue;
 
     /**
      * Constructor.
@@ -52,7 +58,11 @@ public class GuiView extends Application implements View {
      * @return The user input.
      */
     public String getInput() {
-        return "testing";
+        try {
+            return blockingQueue.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -66,40 +76,59 @@ public class GuiView extends Application implements View {
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //blockingQueue = new LinkedBlockingQueue<>();
+        blockingQueue = new LinkedBlockingQueue<>(1);
 
         output = new Label();
+        VBox outputPane = new VBox(output);
+        outputPane.setAlignment(Pos.TOP_CENTER);
 
-        output.setAlignment(Pos.CENTER);
         input = new TextField();
         submitButton = new Button("Enter text");
-        submitButton.setOnAction(e -> {
-            String text = input.getText();
-            input.clear();
-            display(text);
-            //blockingQueue.offer(text);
-            //submitButton.setDisable(true);
-            //if(!blockingQueue.offer(text)) displayError("There was an error reading your input"
-            //        , "Apologies, we could not process your user input at this time.");
-        });
+        submitButton.setOnAction(e -> submitText());
 
-        ScrollPane scrollPane = new ScrollPane(output);
+        ScrollPane scrollPane = new ScrollPane(outputPane);
         scrollPane.setPrefHeight(300);
-        scrollPane.setPrefWidth(300);
-
-        output.heightProperty().addListener((observable, oldValue, newValue) -> {
-            output.layout();
-            scrollPane.setVvalue(1.0d);
-        });
+        scrollPane.setPrefWidth(400);
+        setScrollPaneVBarToBottom(scrollPane);
 
         Pane root = new VBox(scrollPane, new HBox(input, submitButton));
 
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("style.css");
+        scene.getAccelerators().put(new KeyCodeCombination(KeyCode.ENTER), () -> submitButton.fire());
         primaryStage.setTitle("Cinema Booking Application");
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setResizable(false);
         countDownLatch.countDown();
     }
+
+    /**
+     * Snap the scrollPane's VBar to the bottom of the output label.
+     * @param scrollPane The scrollPane that we want to have its VBar at the bottom.
+     */
+    private void setScrollPaneVBarToBottom(ScrollPane scrollPane)
+    {
+        output.heightProperty().addListener((observable, oldValue, newValue) -> {
+            output.layout();
+            scrollPane.setVvalue(1.0d);
+        });
+    }
+
+    /**
+     * Set up the functionality, such that whenever the submit-button is clicked, input is taken from the 'input'
+     * box and is displayed on the screen.
+     */
+    private void submitText()
+    {
+        String text = input.getText();
+        input.clear();
+        display(text);
+
+        if(!blockingQueue.offer(text)) displayError("There was an error reading your input"
+                , "Apologies, we could not process your user input at this time.");
+    }
+
 
     /**
      * Display some text to the user, by appending this text to the end of the 'output label'.
@@ -107,8 +136,8 @@ public class GuiView extends Application implements View {
      * @param text The text to be displayed.
      */
     @Override
-    public void displayWithFormatting(String text) {
-        display(getSeparator());
+    public void displayWithFormatting(String text)
+    {
         display(text);
         display(getSeparator());
     }
@@ -119,7 +148,8 @@ public class GuiView extends Application implements View {
      * @param message The error message to be displayed.
      */
     @Override
-    public void displayError(String message) {
+    public void displayError(String message)
+    {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setContentText(message);
         alert.showAndWait();
