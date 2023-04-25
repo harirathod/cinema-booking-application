b669efc61b4daead7dc6e656057b74c4d9193b74
harirathod <h4ri.rathod@gmail.com>
2023-04-25 18:04:55 +0100

    Create class to allow user to pick Booking type

---
+++ src/main/java/com/cinema/cinema/BookingChooser.java
@@ -0,0 +1,24 @@
+package com.cinema.cinema;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class displays a graphical UI to the user, and lets them select whether they want to use the CustomerBooking,
+ * or the ManagerBooking.
+ * @author hari_rathod
+ */
+public class BookingChooser extends Application {
+
+    /**
+     * Starts the BookingChooser.
+     * @param primaryStage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane
+    }
+}
