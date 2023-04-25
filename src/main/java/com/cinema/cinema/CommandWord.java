8d934b6ea027894d62d73d5f5a1f4e094a95cb38
harirathod <h4ri.rathod@gmail.com>
2023-04-25 18:07:58 +0100

    Add all commands to this class

--- src/main/java/com/cinema/cinema/CommandWord.java
+++ src/main/java/com/cinema/cinema/CommandWord.java
@@ -1,13 +1,55 @@
 package com.cinema.cinema;
 
-public interface CommandWord
-{
-    String getCommandString();
-    String getPlaceholder();
-    boolean hasPlaceholder();
+/**
+ * This enumeration class defines the command words available to the customer.
+ * @author Hari Rathod
+ * @version 2023.03.30
+ */
+public enum CommandWord {
+    HELP("help", null), BOOK("book", "movie title"), LIST("list", "movies"),
+    BASKET("basket", null), SAVE("save", "tickets"), QUIT("finished", null),
+    ADD("add", "movie"), REMOVE("remove", "movie"),
+    UNKNOWN(null, null);
 
+    private String commandString;
+    private String placeholder;
 
-    <E extends CommandWord> E convertToCommandWord(String commandString);
+    /**
+     * Initialise fields.
+     *
+     * @param commandString The main part of the ManagerCommandWord.
+     * @param placeholder   The placeholder.
+     */
+    CommandWord(String commandString, String placeholder) {
 
-    <E extends CommandWord> E getQuitCommand();
+        this.commandString = commandString;
+        this.placeholder = placeholder;
+    }
+
+    /**
+     * Get the command string associated with a command word.
+     *
+     * @return The command string associated with a command word.
+     */
+    public String getCommandString() {
+        return commandString;
+    }
+
+    /**
+     * Get the placeholder associated with a command word. Returns null
+     *
+     * @return The placeholder associated with a command word.
+     */
+    public String getPlaceholder() {
+        return placeholder;
+    }
+
+    /**
+     * Check whether a ManagerCommandWord has an associated placeholder or not.
+     *
+     * @return True if it has a placeholder, false otherwise.
+     */
+    public boolean hasPlaceholder() {
+        return placeholder != null;
+    }
 }
