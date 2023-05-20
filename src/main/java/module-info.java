2a1b286a1e03ceda27ce4a30e080544c14c3628b
harirathod <h4ri.rathod@gmail.com>
2023-05-20 12:13:10 +0100

    Update JAR file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.cinema.cinema {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.fxml;
+    requires org.mongodb.driver.sync.client;
 
 
     opens com.cinema.cinema to javafx.fxml;
