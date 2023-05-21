f39fd3af23d962dd7c78b0b77d6cb5770f7c60f7
harirathod <h4ri.rathod@gmail.com>
2023-05-21 19:06:19 +0100

    Move finishing message from start() to 'case QUIT -> ...'

--- src/main/java/com/cinema/cinema/CustomerBooking.java
+++ src/main/java/com/cinema/cinema/CustomerBooking.java
@@ -55,8 +55,6 @@ public class CustomerBooking extends Booking {
 
         // Start the booking. Continue to process input until the user quits.
         super.start();
-
-        getView().displayWithFormatting("Thanks for visiting, and have a great time!");
     }
 
     /**
@@ -78,7 +76,7 @@ public class CustomerBooking extends Booking {
             case LIST -> list(command);
             case BASKET -> showTickets();
             case SAVE -> saveTickets();
-            case QUIT -> {}
+            case QUIT -> getView().displayWithFormatting("Thanks for visiting, and have a great time!");
             default -> unknown();
         }
     }
