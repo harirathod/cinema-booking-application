272f9aa352568b55d489dfd7d5c0c2cb37eb7fcd
harirathod <h4ri.rathod@gmail.com>
2023-05-21 19:06:33 +0100

    Add documentation

--- src/main/java/com/cinema/cinema/Main.java
+++ src/main/java/com/cinema/cinema/Main.java
@@ -1,7 +1,23 @@
 package com.cinema.cinema;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+/**
+ * This is the main class with the main method that starts the application.
+ * @author hari_rathod
+ * @version 2023.05.21
+ */
 public class Main {
-    public static void main(String[] args) {
+
+    /**
+     * The main method that starts the application.
+     * @param args Optional arguments.
+     */
+    public static void main(String[] args)
+    {
         new CinemaBookingApplication().run(args);
     }
 }
