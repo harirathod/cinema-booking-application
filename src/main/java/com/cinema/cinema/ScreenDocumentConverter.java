815adce6999a92519330401d20f25edbda2d699e
harirathod <h4ri.rathod@gmail.com>
2023-05-30 16:01:31 +0100

    Implement converter methods

--- src/main/java/com/cinema/cinema/ScreenDocumentConverter.java
+++ src/main/java/com/cinema/cinema/ScreenDocumentConverter.java
@@ -1,15 +1,50 @@
 package com.cinema.cinema;
 
+import org.bson.BsonArray;
+import org.bson.BsonBoolean;
+import org.bson.BsonString;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.asList;
 
 public class ScreenDocumentConverter {
     public static Screen convertDocumentToScreen(Document d)
     {
-        return null;
+        ArrayList<ArrayList<Boolean>> seats = (ArrayList<ArrayList<Boolean>>) d.get("seats");
+
+        // Create a screen with the appropriate id and seats.
+        Screen screen = new Screen((Integer) d.get("_id"), seats.size(), seats.get(0).size());
+
+        // If a screen has a screening, set its movie title and ticket cost.
+        if ((Boolean) d.get("hasMovieScreening")) {
+            screen.addNewMovie((String) d.get("movieTitle"), (Integer) d.get("ticketCost"));
+        } else {
+            screen.removeMovie();
+        }
+        return screen;
     }
 
-    public static Document convertScreenToDocument(Screen d)
+    public static Document convertScreenToDocument(Screen screen)
     {
-        return null;
+        Document document = new Document("_id", screen.getId());
+        document.append("hasMovieScreening", screen.hasMovieScreening())
+                .append("movieTitle", screen.getMovieTitle())
+                .append("ticketCost", screen.getTicketCost());
+
+        // Convert the List of seats to a BSON array.
+        BsonArray bsonSeats = new BsonArray();
+        for (boolean[] row : screen.getSeats()) {
+            BsonArray bsonRow = new BsonArray();
+            for (boolean seat : row) {
+                bsonRow.add(new BsonBoolean(true));
+            }
+            bsonSeats.add(bsonRow);
+        }
+        document.append("seats", bsonSeats);
+
+        return document;
     }
 }
