bf4103dc9194afd20db5af86ecda07748f33c275
harirathod <h4ri.rathod@gmail.com>
2023-05-30 15:05:01 +0100

    Create class

---
+++ src/main/java/com/cinema/cinema/ScreenDocumentConverter.java
@@ -0,0 +1,15 @@
+package com.cinema.cinema;
+
+import org.bson.Document;
+
+public class ScreenDocumentConverter {
+    public static Screen convertDocumentToScreen(Document d)
+    {
+        return null;
+    }
+
+    public static Document convertScreenToDocument(Screen d)
+    {
+        return null;
+    }
+}
