d9cb5297c2b580ce3612cc9e9d065a8669c7ea0c
harirathod <h4ri.rathod@gmail.com>
2023-05-30 15:05:42 +0100

    Create class to connect to MongoDB Atlas

--- src/main/java/com/cinema/cinema/ScreenDataManipulator.java
+++ src/main/java/com/cinema/cinema/ScreenDataManipulator.java
@@ -1,22 +1,93 @@
 package com.cinema.cinema;
 
-import java.io.IOException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * This class is used to ensure the ticket and screen files are set up appropriately.
+ * This class interfaces with the MongoDB cloud database storing the screens, and provides methods for updating
+ * screens in the database.
+ * @author hari_rathod
+ * @version 2023.05.30
+ * @see Screen
  */
 public class ScreenDataManipulator {
-    private final ObjectDataRecorder<Screen> screenDataRecorder = new ObjectDataRecorder<>(Filename.SCREEN, Screen.class);
+    private String uri;
 
+    public ScreenDataManipulator()
+    {
+        this.uri = "mongodb+srv://" + System.getProperty("username") + ":" + System.getProperty("password") + "@cluster1.y3fcbqx.mongodb.net/?retryWrites=true&w=majority";
+    }
     /**
-     * Initialise fields. Ensure the ticket and screen files are reset.
-     * @throws IOException If there was an error handling the files.
+     * Get all screens from the database.
+     * @return A list of all the screens.
      */
-    public ScreenDataManipulator() throws IOException {
-        screenDataRecorder.resetFile();
+    public List<Screen> getAllScreens()
+    {
+        List<Screen> screens = new ArrayList<>();
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            // Connect to the MongoDB database 'cinema', and get collection 'screens'.
+            MongoCollection<Document> documentScreens = mongoClient.getDatabase("cinema").getCollection("screens");
+            for (Document doc : documentScreens.find()) {
+                System.out.println(doc.get("_id"));
+                System.out.println(doc.get("movieTitle"));
+                screens.add(ScreenDocumentConverter.convertDocumentToScreen(doc));
+            }
+        }
+
+        return screens;
     }
 
-    public void addScreen(Screen screen) throws IOException, ClassNotFoundException {
-        screenDataRecorder.writeToFile(screen);
+    /**
+     * Get the screen with matching id.
+     * @param id The id of the screen we want to retrieve.
+     */
+    public Screen getScreenById(int id)
+    {
+        return null;
+    }
+
+    /**
+     * Record the screen in the database. Overwrites the existing screen with matching id, if there is a matching id,
+     * otherwise adds the provided screen as a 'new screen'.
+     * @param screen The screen to be recorded.
+     */
+    public void recordScreen(Screen screen)
+    {
+
+    }
+
+    /**
+     * Update the movie screening in a screen.
+     * @param id The id of the screen to be updated.
+     * @param movieTitle The title of the movie.
+     * @param ticketCost The cost of a ticket to watch this movie.
+     */
+    public void updateScreening(int id, String movieTitle, int ticketCost)
+    {
+
+    }
+
+    /**
+     * Remove the movie screening from a screen.
+     * @param id The id of the screen to remove the movie screening from.
+     */
+    public void removeScreening(int id)
+    {
+
+    }
+
+    /**
+     * Delete a screen from the database.
+     * @param id The id of the screen that is to be removed (deleted).
+     */
+    public void deleteScreen(int id)
+    {
+
     }
 }
