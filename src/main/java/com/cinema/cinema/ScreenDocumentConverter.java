c1d937d9c2cafdd01c64e66b1b91bd49580c86ad
harirathod <h4ri.rathod@gmail.com>
2023-06-09 15:33:25 +0100

    Add bug fix that meant booked seats weren't being recorded

--- src/main/java/com/cinema/cinema/ScreenDocumentConverter.java
+++ src/main/java/com/cinema/cinema/ScreenDocumentConverter.java
@@ -10,13 +10,21 @@ import java.util.Arrays;
 
 import static java.util.Arrays.asList;
 
-public class ScreenDocumentConverter {
+public class ScreenDocumentConverter
+{
     public static Screen convertDocumentToScreen(Document d)
     {
-        ArrayList<ArrayList<Boolean>> seats = (ArrayList<ArrayList<Boolean>>) d.get("seats");
+        ArrayList<ArrayList<Boolean>> docSeats = (ArrayList<ArrayList<Boolean>>) d.get("seats");
+        boolean[][] seats = new boolean[docSeats.size()][docSeats.get(0).size()];
+        for (int i = 0; i < seats.length; i++) {
+            for (int j = 0; j < seats[0].length; j++) {
+                seats[i][j] = docSeats.get(i).get(j);
+            }
+        }
 
         // Create a screen with the appropriate id and seats.
-        Screen screen = new Screen((Integer) d.get("_id"), seats.size(), seats.get(0).size());
+        Screen screen = new Screen((Integer) d.get("_id"), docSeats.size(), docSeats.get(0).size());
+        screen.setSeats(seats);
 
         // If a screen has a screening, set its movie title and ticket cost.
         if ((Boolean) d.get("hasMovieScreening")) {
@@ -24,6 +32,7 @@ public class ScreenDocumentConverter {
         } else {
             screen.removeMovie();
         }
+
         return screen;
     }
 
@@ -33,7 +42,6 @@ public class ScreenDocumentConverter {
         document.append("hasMovieScreening", screen.hasMovieScreening())
                 .append("movieTitle", screen.getMovieTitle())
                 .append("ticketCost", screen.getTicketCost());
-
         // Convert the List of seats to a BSON array.
         BsonArray bsonSeats = new BsonArray();
         for (boolean[] row : screen.getSeats()) {
